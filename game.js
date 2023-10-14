a318c3699e7c162ab42cc31ed9d1d9e26780835a
Britojaison <britojaison123@gmail.com>
2023-10-14 20:50:40 +0530

    moved portfolio to new folder and added simon game files

---
+++ game.js
@@ -0,0 +1,83 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+//console.log(buttonColours);
+var started = false;
+var level = 0;
+$(document).keydown(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").click(function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    //console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentlevel) {
+    if (gamePattern[currentlevel] === userClickedPattern[currentlevel]) {
+        console.log("succuss");
+
+        if (gamePattern.length === userClickedPattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("wrong");
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){$("body").removeClass("game-over")},200);
+        $("#level-title").text("press any key to start");
+        //alert("YOu lose at level:"+level);
+        startOver();
+    }
+
+}
+
+function startOver(){
+    level=0;
+    gamePattern=[];
+    started=false;
+}
+
+function nextSequence() {
+    userClickedPattern=[];
+    level++;
+
+    $("#level-title").text("Level " + level);
+    var num = Math.random() * 4;
+    var randomNumber = Math.floor(num);
+    var randomChosenColour = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColour);
+    //console.log(gamePattern);
+
+    $("#" + buttonColours[randomNumber]).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+
+
+
